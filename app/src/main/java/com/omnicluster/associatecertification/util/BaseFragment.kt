495832f6691dba45d0b5d4d4e7a8ba7c70f60e5a
TonaMorgan <tonatiuh.morgan@banregio.com>
2020-07-24 20:59:42 -0500

    Crear fragmento base para inflado de layout por ID

---
+++ app/src/main/java/com/omnicluster/associatecertification/util/BaseFragment.kt
@@ -0,0 +1,26 @@
+package com.omnicluster.associatecertification.util
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+
+/**
+ *
+ * Base fragment that inflates de layout given in the constructor.
+ *
+ * @param fragmentLayout [Int] resource id for the layout that will be inflated.
+ * */
+abstract class BaseFragment(@LayoutRes private val fragmentLayout: Int) : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(fragmentLayout, container, false)
+    }
+
+}
